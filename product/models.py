82c13a8ba20ab3ae301fb8851ea3573cb5e487fa
Siddharth <siddharthabadal.2015@gmail.com>
2022-09-16 19:47:42 +0530

    changes made

--- product/models.py
+++ product/models.py
@@ -65,4 +65,10 @@ class Product(models.Model):
     
     
 
-   
+class ProductImages(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="staticfiles/admin/img/")
+
+
+    def __str__(self):
+        return self.product.name
